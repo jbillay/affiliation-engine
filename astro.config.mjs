af21b66f1d8d154838fee8ac437703f49cf30062
Jeremy Billay <jbillay@gmail.com>
2025-05-25 12:42:52 +0200

    Update projet structure and add GitHub Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://jbillay.github.io',
-  base: '/affiliation-engine',
-})
+  site: "https://jbillay.github.io",
+  base: "/affiliation-engine",
+  integrations: [mdx(), sitemap()],
+});
